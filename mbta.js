d4552d90a53da9b633b419e963cc8f63e9d2d57e
Najwab <inajjwa2@gmail.com>
2020-01-26 18:22:41 +0300

    solved V2

--- mbta.js
+++ mbta.js
@@ -4,65 +4,54 @@ const mbta=[
 {Orange:['North Station','Haymarket','Park Street','State','Downtown Crossing','Chinatown','Back Bay','Forest Hills']},
 ];
 
+
+
+
+
+
  const stopsBetweenStations=function(sLine , stationNS , eLine ,stationNE ){
     let stop =0;
     for(let i=0;i<mbta.length;i++){
-        //console.log(mbta[i]);
         for(const key in mbta[i]){
-            //console.log(mbta[i][key]);
             let keyy = mbta[i][key]; 
-           // for(let j =0 ; j<keyy.length;j++){
-                // console.log(mbta[i][key][j]);
-                // console.log(keyy[j]);
-                
-                
                  if(sLine === eLine ){
                     if(stationNS === stationNE){
                         stop=0;
                     }else if(stationNS !== stationNE){
-                        let startS=keyy.indexOf(stationNS); //0  //7 //3
-                        let endS=keyy.indexOf(stationNE);// 0   //0  //5
-                        let result=startS-endS; 
-                       result=Math.abs(result);
+                        let startS=keyy.indexOf(stationNS); 
+                        let endS=keyy.indexOf(stationNE);
+                        let result=Math.abs(startS-endS); 
                        stop+=result;
                     }
                 }else if(sLine !== eLine){
                     if(stationNS === stationNE){
                         stop=0;
                     }else if (stationNS !== stationNE){
-                        let startS=mbta[i][sLine].indexOf(stationNS);
-                        let finalStart=0;
-                        let earlystarting=mbta[i][sLine].indexOf("Park Street");
-                        if(startS>earlystarting){
-                            for(let j =startS;j>earlystarting;j--)
-                            {finalStart +=1;}
-
-                        }else{
-                             finalStart=+1
+                            let startingPoint=mbta[i][sLine].indexOf(stationNS);
+                            let parkStarting=mbta[i][sLine].indexOf("Park Street");
+                             let finalStart=Math.abs(startingPoint-parkStarting);
+                        
+                        
+                            let endingPoint=mbta[i][eLine].indexOf(stationNE);
+                            let parkending=mbta[i][eLine].indexOf("Park Street");
+                              let finalEnd=Math.abs(endingPoint-parkending);
+                        
+                             stop =finalStart+finalEnd;
                         }
-
-                        let endS=keyy.indexOf(stationNE);
-                        console.log(endS);
-                        let finalEnd=0;
-                        let earlyEnding=mbta[i][eLine].indexOf("Park Street");
-                        if(endS>earlystarting){
-                            for(let j =endS;j>earlyEnding;j--)
-                            {finalEnd +=1;}
-
-                        }else{
-                            finalEnd=+1
-                        }
-
-                      let result=finalStart+finalEnd;
-                       stop+=result;
-                    }
                 }
-
-    //}
     } 
 }return stop;
 }
 
-stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife') // 0 stops +ok
-stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station') // 7 stops +ok
-stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stop
+
+
+
+
+
+
+
+
+stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife') // 0 stops work
+stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station') // 7 stops  work
+stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stop 
+
