75b22cd81587ce7a2a22b04212349596ef419075
Najwab <inajjwa2@gmail.com>
2020-01-27 17:44:08 +0300

    completed

--- mbta.js
+++ mbta.js
@@ -1,57 +1,38 @@
-const mbta=[
-{Red:['South Station','Park Street','Kendall','Central','Harvard','Porter','Davis','Alewife']},
-{Green:['Government Center','Park Street','Boylston','Arlington','Copley','Hynes','Kenmore']},
-{Orange:['North Station','Haymarket','Park Street','State','Downtown Crossing','Chinatown','Back Bay','Forest Hills']},
-];
+const mbta=
+{
+Red:['South Station','Park Street','Kendall','Central','Harvard','Porter','Davis','Alewife'],
+Green:['Government Center','Park Street','Boylston','Arlington','Copley','Hynes','Kenmore'],
+Orange:['North Station','Haymarket','Park Street','State','Downtown Crossing','Chinatown','Back Bay','Forest Hills']
+};
 
-
-
-
-
-
- const stopsBetweenStations=function(sLine , stationNS , eLine ,stationNE ){
+const stopsBetweenStations=function(sLine , stationNS , eLine ,stationNE ){
     let stop =0;
-    for(let i=0;i<mbta.length;i++){
-        for(const key in mbta[i]){
-            let keyy = mbta[i][key]; 
+    let startingPoint=mbta[sLine].indexOf(stationNS);
+    let parkStarting=mbta[sLine].indexOf("Park Street");
+    let endingPoint=mbta[eLine].indexOf(stationNE);
+    let parkending=mbta[eLine].indexOf("Park Street");
+
+
                  if(sLine === eLine ){
                     if(stationNS === stationNE){
                         stop=0;
                     }else if(stationNS !== stationNE){
-                        let startS=keyy.indexOf(stationNS); 
-                        let endS=keyy.indexOf(stationNE);
-                        let result=Math.abs(startS-endS); 
+                        let result=Math.abs(startingPoint-endingPoint); 
                        stop+=result;
                     }
                 }else if(sLine !== eLine){
                     if(stationNS === stationNE){
                         stop=0;
                     }else if (stationNS !== stationNE){
-                            let startingPoint=mbta[i][sLine].indexOf(stationNS);
-                            let parkStarting=mbta[i][sLine].indexOf("Park Street");
                              let finalStart=Math.abs(startingPoint-parkStarting);
-                        
-                        
-                            let endingPoint=mbta[i][eLine].indexOf(stationNE);
-                            let parkending=mbta[i][eLine].indexOf("Park Street");
                               let finalEnd=Math.abs(endingPoint-parkending);
-                        
                              stop =finalStart+finalEnd;
                         }
-                }
-    } 
-}return stop;
 }
-
-
-
-
-
-
-
-
+return stop;
+};
 
 stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife') // 0 stops work
 stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station') // 7 stops  work
-stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stop 
+stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore') // 6 stop work *yeeeeeeeees  thank you :)*
 
