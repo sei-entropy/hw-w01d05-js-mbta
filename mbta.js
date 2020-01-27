3b4bb50bd74aa850452dbda6f08e7687ade24def
hayaAbdulaziz <hayal5066@gmail.com>
2020-01-28 00:15:42 +0300

    h=HWupdate

--- mbta.js
+++ mbta.js
@@ -1,36 +1,47 @@
-const subway ={
-    Red: ['South Station'
-    , 'Park Street',
-     'Kendall', 
-     'Central', 
-    'Harvard', 
-    'Porter', 
-    'Davis', 
-    'Alewife'],
-   Green:  ['Government Center', 
-   'Park Street',
-    'Boylston', 
-    'Arlington',
-     'Copley',
-      'Hynes', 
-      'Kenmore'],
-   Orange: ['North Station','Haymarket', 
-    'Park Street', 
-    'State',
-     'Downtown Crossing',
-      'Chinatown','Back Bay', 'Forest Hills']
-   }
+
+const subWayLine={
+  Red : [
+"South Station",
+"Park Street",
+"Kendall",
+"Central",
+"Harvard",
+"Porter",
+"Davis",
+"Alewife"
+],
+Green: [
+"Government Center",
+"Park Street",
+"Boylston",
+"Arlington",
+"Copley",
+"Hynes",
+"Kenmore"
+],
+
+ Orange : [
+     "North Station",
+     "Haymarket",
+     "Park Street",
+     "State",
+     "Downwn Crossing",
+     "Chinatown",
+     "Back Bay",
+     "Forest Hills"
+ ]
+};
+
    const stopsBetweenStations = function (startLine, startStation, endLine, endStation) {
-  const indexStartStation = subway[startLine].indexOf(startStation)
-  const indexEndStation = subway[endLine].indexOf(endStation)
+  const indexStartStation = subWayLine[startLine].indexOf(startStation)
+  const indexEndStation = subWayLine[endLine].indexOf(endStation)
   if (startLine === endLine) {
-    return Math.abs(indexEndStation - indexStartStation)
+    return Math.abs(indexStartStation - indexEndStation)
   }
-    const indexParkStreetSl = subway[startLine].indexOf('Park Street');
-    const indexParkStreetEl = subway[endLine].indexOf('Park Steet');
+    const indexParkStreetSl = subWayLine[startLine].indexOf("Park Street");
+    const indexParkStreetEl = subWayLine[endLine].indexOf("Park Street");
     const startLineDist = Math.abs(indexParkStreetSl - indexStartStation);
     const endlineDist = Math.abs(indexParkStreetEl - indexEndStation);
   const totel=startLineDist+endlineDist;
     return totel;
   }
-
