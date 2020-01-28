cded6bd8326e96f8611e6dc9e0fb7ffe8395ff70
Rawan991 <rawan.alrajeh@gmail.com>
2020-01-28 18:51:41 +0300

    resolve hw

--- mbta.js
+++ mbta.js
@@ -1,23 +1,36 @@
+
+
 const subwayLines = {
     'Red': ['South Station', 'Park Street', 'Kendall', 'Central', 'Harvard', 'Porter', 'Davis', 'Alewife'],
     'Green': ['Government Center', 'Park Street', 'Boylston', 'Arlington', 'Copley', 'Hynes', 'Kenmore '],
-    'Orange': ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
+    'Orange': ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crocssing', 'Chinatown', 'Back Bay', 'Forest Hills']
 };
 
 const stopsBetweenStations = function (startLine, startStation, endLine, endStation) {
-    startLine = lettersCase(startLine);
+  
+     startLine = lettersCase(startLine);
     startStation = lettersCase(startStation);
     endLine = lettersCase(endLine);
     endStation = lettersCase(endStation);
-    let startIndex = subwayLines[startLine].indexOf(startStation)
-    let endIndex = subwayLines[endLine].indexOf(endStation)
+    
     if (startLine === endLine) {
-        let stop = Math.abs(startIndex - endIndex)
+       let startIndex = subwayLines[startLine].indexOf(startStation)
+        let endIndex = subwayLines[endLine].indexOf(endStation)
+        let stop = Math.abs( startIndex - endIndex ) 
         return stop;
-    } else {
-        startPark = subwayLines[startLine].indexOf('Park Street')
-        endPark = subwayLines[endLine].indexOf('Park Street')
-        let stop = Math.abs(startIndex - startPark) + Math.abs(endIndex - endPark)
+
+    } 
+    
+    else {
+
+        let ofPark = subwayLines[startLine].indexOf('Park Street')
+        let firstStop = subwayLines[startLine].indexOf(startStation)
+
+        let firstPark = Math.abs( firstStop - ofPark )
+        let indexFinalStop = subwayLines[endLine].indexOf(endStation)
+        let finalPark = Math.abs(ofPark - indexFinalStop )
+
+        let stop = firstPark + finalPark 
         return stop
 
     }
@@ -40,7 +53,26 @@ const lettersCase = function (area){
     }
     return newArea;
 }
+const validation = function (startLine, startStation, endLine, endStation) {
+     if (!Object.keys(subwayLines).includes(startLine)) {
+        return "Wrong start line.."
+    }
 
-console.log(stopsBetweenStations(' red ', 'south Station', 'orange', 'North Station')+ ' stops' );
+    if (!Object.keys(subwayLines).includes(endLine)) {
+        return "Wrong end line.."
+    }
+
+    if (!subwayLines[startLine].includes(startStation)) {
+        return "Wrong start station.."
+    }
+
+    if (!subwayLines[endLine].includes(endStation)) {
+        return "Wrong end station.."
+    }
+}
+
+console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife')+ ' stops' );
+console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station')+ ' stops' );
+console.log(validation('Red', 'Alewife', 'Red', 'South Station'))
 
 
